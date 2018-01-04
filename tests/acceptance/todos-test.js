909e00f687bc50e32e78da76a359ffeafebfa66b
Sean Clayton <clayton.sean@gmail.com>
2018-01-04 13:17:49 -0500

    Add empty acceptance tests

--- tests/acceptance/todos-test.js
+++ tests/acceptance/todos-test.js
@@ -1,12 +1,19 @@
-import { test } from 'qunit';
+import { skip } from 'qunit';
 import moduleForAcceptance from 'ember-todo/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | todos');
 
-test('visiting /todos', function(assert) {
-  visit('/todos');
+skip('should todos on the homepage', () => {});
 
-  andThen(function() {
-    assert.equal(currentURL(), '/todos');
-  });
-});
+skip('should show count of total items', () => {});
+
+skip('should be able to add an item to the todo list', () => {});
+
+skip('should be able to delete an item from the list', () => {});
+
+skip('should be able to edit an item from the list', () => {});
+
+skip('should be able to search/filter for items in todo list', () => {});
+
+// Maybe later
+skip('should be able to sort by date', () => {});
