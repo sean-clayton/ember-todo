7e8438654ee609cd108b0f7579abc4737aabd680
Sean Clayton <clayton.sean@gmail.com>
2018-01-10 10:27:11 -0500

    Add translations

---
+++ app/locales/en/translations.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'Todo List',
+  number_of_todos: {
+    one: '{{count}} Todo',
+    other: '{{count}} Todos'
+  },
+  delete_all_todos: 'Delete All Todos',
+  add_todo: 'Add Todo',
+  no_todos: 'No todos!',
+  edit: 'Edit',
+  delete: 'Delete',
+  save: 'Save',
+  cancel: 'Cancel'
+};
--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
+    defaultLocale: 'en',
     modulePrefix: 'ember-todo',
     environment,
     rootURL: '/',
