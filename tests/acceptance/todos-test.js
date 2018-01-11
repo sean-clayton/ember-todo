f6a2b7b57e5a6af0caed2db881bbe8e8ba8f82fe
Sean Clayton <clayton.sean@gmail.com>
2018-01-11 11:01:12 -0500

    Remove unused import

--- tests/acceptance/todos-test.js
+++ tests/acceptance/todos-test.js
@@ -1,4 +1,4 @@
-import { test, skip } from 'qunit';
+import { test } from 'qunit';
 import moduleForAcceptance from 'ember-todo/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | todos');
